8317591ebb4460e8ba6c4881cd84987bce72d4fb
kalpovskii_a <aserenru@gmail.com>
2023-12-15 16:36:39 +0300

    :heavy_plus_sign: add nextui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import type { AppProps } from "next/app";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
 import { infuraProvider } from "wagmi/providers/infura";
+import { NextUIProvider } from "@nextui-org/react";
+import * as React from "react";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [sepolia],
@@ -27,9 +29,11 @@ const wagmiConfig = createConfig({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
+      <NextUIProvider>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </NextUIProvider>
     </WagmiConfig>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
